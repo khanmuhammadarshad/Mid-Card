aa283dbb6d578e77d2f26a5e822fe14e6e0e01b3
khanmuhammadarshad <arshad.khan@intellexalsolutions.com>
2020-11-17 13:59:09 +0500

    All Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,111 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/arshad.jpg'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "Arshad Khan",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade100,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: 'Source',
+                      letterSpacing: 2.5),
+                ),
+                SizedBox(
+                  height: 10.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'khan@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'khan@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.teal.shade900, child: Icon(Icons.add)),
       ),
-    ),
-  );
+    );
+  }
 }
+
+//
+// Row(
+// children: [
+// Icon(
+// Icons.email,
+// color: Colors.teal,
+// ),
+// SizedBox(
+// width: 10.0,
+// ),
+
+// ],
+// ),
